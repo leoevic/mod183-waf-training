6842e0949f89903c7ec1b500a279a21afced0c42
Leonardo Evic <evicleonardo@gmail.com>
2026-02-10 10:42:40 +0100

    add config for apache2 (correct config)

--- develop/public/index.php
+++ develop/public/index.php
@@ -1,3 +1,21 @@
 <?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
 
-phpinfo();
+require(__DIR__ . "/../vendor/autoload.php");
+
+$app = AppFactory::create();
+$app->addRoutingMiddleware();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Dies ist ein Test");
+    return $response;
+});
+
+$app->get('/windows/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world");
+    return $response;
+});
+
+$app->run();
